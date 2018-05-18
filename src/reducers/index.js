a6cc0d7f76b02782b88eaa6fbd5141a1c6a7088a
Sasheem <sts12@me.com>
2018-05-18 14:00:08 -0400

    made first reducer and hardcoded data in json file

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,5 +1,27 @@
 import { combineReducers } from 'redux';
+import LibraryReducer from 'LibraryReducer';
 
 export default combineReducers({
-  libraries: () => []
+  libraries: LibraryReducer
 });
+
+
+// rememebr a reducer is a function that returns some amount of data
+// this data represents the states for our App
+/*
+  Reducer Design
+  Different types of reducers
+  How do we know which reducers to make?
+
+  we will have a list of libraries and a current library
+  for this application lets make two pieces of states
+  thus we'll need two separate reducers
+  libraryReducer
+    producing our list of libraries to the user
+    an array of objects
+    id and name
+  selectionReducer
+    keeps track of current library to be expanded for more detail
+    id
+
+*/

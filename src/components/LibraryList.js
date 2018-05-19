451a91fbb904ef36df135b7dbc5b148f5770a6c4
Sasheem <sts12@me.com>
2018-05-18 20:02:56 -0400

    added summary of what has been done so far

--- src/components/LibraryList.js
+++ src/components/LibraryList.js
@@ -17,3 +17,19 @@ const mapStateToProps = state => {
 // when connect is called, it returns another function
 // we then immediately call that function with a LibraryList
 export default connect(mapStateToProps)(LibraryList);
+
+
+/*
+  Summary
+  when app first boots up redux creates a new store using the libraries reducer
+    once store is created, it runs the reducer one time
+      each object returned by the reducer
+        we pass it to the provider as a prop - provider is the middle man between react and redux
+        next App component is rendered to screen
+          thus renders LibraryList component
+            once LibraryList is about to render, the connect helper fires
+              "the component im wrapping is about to render"
+            the connect function then reaches to the provider and gets the state
+            provider gives up the state and the connect tag pumps it down into the library list
+                done via mapStateToProps
+*/
